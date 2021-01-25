fd16d489bdc8e37db64d03a3d22cf84225f904c5
soni.niket29@gmail.com <nikz@0133>
2021-01-25 21:10:28 +0530

    first

---
+++ Controllers/WorksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Niketsoni.Models;
+
+namespace Niketsoni.Controllers
+{
+    public class WorksController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
