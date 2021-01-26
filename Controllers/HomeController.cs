f15338e044229876bae7f920a28a71b29e732c28
Shelbin <shelbin.kidangan@gmail.com>
2021-01-26 20:23:13 +0530

    m

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Niketsoni.Services;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Niketsoni.Models;
 
 namespace Niketsoni.Controllers
 {
     public class HomeController : Controller
     {
-
+        private readonly IEmailSender _emailSender;
+        public HomeController( IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost("api/RequestDemo")]
+        public async Task<IActionResult> RequestDemo([FromBody] RequestDemo requestDemo)
+        {
+            var body = $"Subject: {requestDemo.Name}. Email: {requestDemo.Email}. Message: {requestDemo.Description}";
+            await _emailSender.SendEmailAsync("", "Demo Request", body);
+            return Ok();
+        }
+    }
+    public class RequestDemo
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
     }
 }
